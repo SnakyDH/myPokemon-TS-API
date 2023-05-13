3d9e5520a32b7a2f97a824f2b756e35e753b9161
snakydh <skdanielg5@gmail.com>
2023-05-12 20:54:14 -0500

    [UPDATE] improving controller

--- src/controllers/poke.controller.ts
+++ src/controllers/poke.controller.ts
@@ -1,29 +1,30 @@
 import { Request, Response } from "express";
 import { Pokemon } from "../models/Pokemon";
-import { Type } from "../models/ResponseAPI";
+import { Type, Sprites } from "../models/ResponseAPI";
 
 const urlBase: string = "https://pokeapi.co/api/v2/pokemon/";
 
 const getMyPokemon = async (req: Request, res: Response) => {
   try {
-    const { name } = req.query;
-    if (name === undefined) {
-      return res.status(400).json({ message: "Bad Request" });
-    }
+    const { name } = req.params;
     const url = `${urlBase}${name}`;
     const response = await fetch(url, { method: "GET" });
     if (response.status === 200) {
       const myData = await response.json();
       const arrayTypes = myData.types;
-      console.log(arrayTypes);
-      const myTypes = arrayTypes.map((objType: Type) => objType.type.name);
-      const myPokemon = new Pokemon(
-        myData.id,
-        myData.name,
-        myData.sprites,
-        myTypes
+      const myTypes: Array<string> = arrayTypes.map(
+        (objType: Type) => objType.type.name
       );
-      return res.status(200).json({ message: "OK", myPokemon: myPokemon });
+      // parsing
+      const dataId: number = myData.id as number;
+      const dataName: string = myData.name as string;
+      const dataSprites: Sprites = myData.sprites as Sprites;
+      // delete unnesesary
+      delete dataSprites.other;
+      delete dataSprites.versions;
+
+      const myPokemon = new Pokemon(dataId, dataName, dataSprites, myTypes);
+g      return res.status(200).json({ message: "OK", myPokemon: myPokemon });
     } else if (response.status === 404) {
       return res.status(404).json({ message: "Not Found" });
     } else {
