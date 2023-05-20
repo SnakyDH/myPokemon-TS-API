f85b2e319087139021f89ff3c6e0c11d68c694ce
snakydh <skdanielg5@gmail.com>
2023-05-20 16:39:09 -0500

    [FIX] cors problems

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express, { Express } from "express";
 import pokeRouter from "./routes/poke.routes";
-
+import cors from "cors";
 const app: Express = express();
 const port: number = Number(process.env.PORT) | 3000;
+
+var corsOptions = {
+  origin: "https://my-pokemon-ts-react.vercel.app/",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use(cors(corsOptions));
 
 app.use("/api", pokeRouter);
 
